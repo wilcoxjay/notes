a4c5ed7593b9e527ee049952218369cd74a6131b
James Wilcox <jrw12@cs.washington.edu>
2017-05-19 17:18:34 -0700

    [RegularGrammars] add some doc; use powerset

--- RegularGrammars.v
+++ RegularGrammars.v
@@ -12,6 +12,8 @@ Require Import Coq.Program.Program.
 Set Implicit Arguments.
 Set Maximal Implicit Insertion.
 
+(* First, some library helper functions and notations. *)
+
 Obligation Tactic := unfold complement, equiv ; program_simpl.
 Program Instance option_eqdec A `(EqDec A eq) : EqDec (option A) eq :=
 {
@@ -36,6 +38,11 @@ Definition filterMap {A B} (f : A -> option B) : list A -> list B :=
 
 Notation "x |> f" := (f x) (left associativity, at level 98, only parsing) .
 
+
+(* A type representing valid right-hand sides of left-regular grammar rules.
+   The original email used a much looser representation of rules, which did not
+   separate the LHS from the RHS, and which did not enforce regularity. By
+   restricting the representation, we make it easier to write a parser. *)
 Module rhs.
   Inductive t T NT :=
   | Empty : t T NT
@@ -60,7 +67,6 @@ Module rhs.
 End rhs.
 Import rhs.exports.
 
-
 Module reg_grammar.
   Section reg_grammar.
     Variable T NT : Type.
@@ -71,8 +77,16 @@ Module reg_grammar.
       rules : list(NT * rhs.t T NT)
   }.
 
-  (* We track a set of nonterminal symbols we're currently in,
-     with the additonal special symbol None representing "end of string". *)
+  (* Next, we're going to write a function [parse] that decides whether a string
+     is in the language represented by the grammar. The parser keeps track of
+     the set of nonterminal symbols it's currently in, with the additonal
+     special symbol None representing "end of string when the last rule applied
+     had RHS [Single]". *)
+
+  (* It may help to scroll down to the function [parse] first, and read
+     backwards up to here. *)
+
+  (* Given the RHS of a rule and a terminal, decides whether the RHS can be used. *)
   Definition step_rhs (t : T) (rhs : rhs.t T NT) : list (option NT) :=
     match rhs with
     | Empty => []
@@ -80,21 +94,25 @@ Module reg_grammar.
     | Continue t' nt => if t == t' then [Some nt] else []
     end.
   
-  
+  (* Finds all the productions for a given nonterminal. *)
   Definition getRHS T NT `{EqDec NT eq}
            (nt : NT) : list (NT * rhs.t T NT) ->
                        list (rhs.t T NT) :=
     filterMap (fun rule => let '(nt', rhs) := rule in
                         if nt == nt' then Some rhs else None).
   
+  (* Given a nonterminal [nt], applies all possible rules. *)
   Definition step_nt (rules : list(NT * rhs.t T NT)) (t : T) (nt : NT) : list (option NT) :=
     rules |> getRHS nt
           |> flat_map (step_rhs t).
   
+  (* Given a *list* of nonterminals, takes all possible next steps. *)
   Definition step (rules : list(NT * rhs.t T NT)) (t : T) (acc : list NT) : list (option NT) :=
     acc |> flat_map (step_nt rules t)
         |> nodup equiv_dec.
   
+  (* The main parser loop. Repeatedly steps the current set of states using
+     terminals from the string. *)
   Definition parse' (rules : list(NT * rhs.t T NT))
            : list T -> list (option NT) -> list (option NT) :=
     fix rec l acc :=
@@ -106,6 +124,9 @@ Module reg_grammar.
             |> rec l
       end.
 
+  (* Checks to see if the current state represents an accepting state.  In this
+     representataion of state, a state is accepting if it contains [None] or if
+     it contains [Some nt] and there is a rule [(nt, Empty)].  *)
   Definition is_final (rules : list (NT * rhs.t T NT)) (l : list (option NT)) : bool :=
     existsb (fun o => match o with
                    | None => true
@@ -113,6 +134,8 @@ Module reg_grammar.
                    end)
             l.
 
+  (* Top-level parse function. Calls [parse'] with the initial symbol and checks
+     to see whether the resulting state is accepting. *)
   Definition parse (grammar : reg_grammar.t) (l : list T):=
     [Some (start_symbol grammar)]
         |> parse' (rules grammar) l
@@ -134,10 +157,11 @@ Module dfa.
 
     Definition run (m : t) (l : list A) : bool :=
       is_final m (run' (next m) l (initial_state m)).
-
   End dfa.
 End dfa.
 
+(* We can explicitly construct a DFA corresponding to the grammar. In fact, all
+   the hard work was already done in our grammar parser. *)
 Module powerset_construction.
   Section powerset_construction.
     Variable T NT : Type.
@@ -159,6 +183,13 @@ Module powerset_construction.
   End powerset_construction.
 End powerset_construction.
 
+(* A simple example language over the alphabet {a,b} corresponding to the
+   regular expression
+
+       a*b*
+
+   (Note that the original email contained an incorrect grammar for this
+   language. A correct one is given below.) *)
 Module a_b_example.
   Module non_terminal.
     Inductive t:Type :=
@@ -190,16 +221,16 @@ Module a_b_example.
 
   Definition a_b_rules: list(non_terminal.t * rhs.t terminal.t non_terminal.t):=
     [(non_terminal.A, Continue terminal.a non_terminal.A);
-       (non_terminal.A, Continue terminal.b non_terminal.B);
-       (non_terminal.A, Empty);
-       (non_terminal.B, Continue terminal.b non_terminal.B);
-       (non_terminal.B, Empty)].
-
+     (non_terminal.A, Continue terminal.b non_terminal.B);
+     (non_terminal.A, Empty);
+     (non_terminal.B, Continue terminal.b non_terminal.B);
+     (non_terminal.B, Empty)].
 
   Definition a_b_grammar : reg_grammar.t terminal.t non_terminal.t :=
     {| reg_grammar.start_symbol := non_terminal.A;
        reg_grammar.rules := a_b_rules |}.
 
+  (* A few examples. *)
   Eval compute in reg_grammar.parse a_b_grammar [].
   Eval compute in reg_grammar.parse a_b_grammar [terminal.a].
   Eval compute in reg_grammar.parse a_b_grammar [terminal.a; terminal.a].
@@ -208,6 +239,7 @@ Module a_b_example.
   Eval compute in reg_grammar.parse a_b_grammar [terminal.b; terminal.a].
 
 
+  (* A hand rolled DFA for the same language. *)
   Definition a_b_next (s : option non_terminal.t) (t : terminal.t) : option non_terminal.t :=
     match s with
     | None => None
@@ -235,10 +267,22 @@ Module a_b_example.
        dfa.next := a_b_next
     |}.
 
+  (* Examples running the DFA. *)
   Eval compute in dfa.run a_b_dfa [].
   Eval compute in dfa.run a_b_dfa [terminal.a].
   Eval compute in dfa.run a_b_dfa [terminal.a; terminal.a].
   Eval compute in dfa.run a_b_dfa [terminal.b; terminal.b].
   Eval compute in dfa.run a_b_dfa [terminal.a; terminal.b].
   Eval compute in dfa.run a_b_dfa [terminal.b; terminal.a].
+
+  (* Automatically construct a DFA using the powerset construction. *)
+  Definition a_b_dfa' := powerset_construction.dfa a_b_grammar.
+
+  (* Examples running the second DFA. *)
+  Eval compute in dfa.run a_b_dfa' [].
+  Eval compute in dfa.run a_b_dfa' [terminal.a].
+  Eval compute in dfa.run a_b_dfa' [terminal.a; terminal.a].
+  Eval compute in dfa.run a_b_dfa' [terminal.b; terminal.b].
+  Eval compute in dfa.run a_b_dfa' [terminal.a; terminal.b].
+  Eval compute in dfa.run a_b_dfa' [terminal.b; terminal.a].
 End a_b_example.
